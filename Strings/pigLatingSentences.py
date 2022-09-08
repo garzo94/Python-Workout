a6ac31bfc494dfc9ae157b48971062aa2be16bf7
garzo94 <alexgarzo25@gmail.com>
2022-09-07 19:00:25 -0600

    adding book solution

--- Strings/pigLatingSentences.py
+++ Strings/pigLatingSentences.py
@@ -12,3 +12,12 @@ def pig_latin(sentence):
 pig_latin('this is a test translation')
 
 #book solution
+# def pl_sentence(sentence):
+#     output = []
+#     for word in sentence.split():
+#         if word[0] in 'aeiou':
+#             output.append(f'{word}way')
+#         else:
+#             output.append(f'{word[1:]}{word[0]}ay')
+#     return ' '.join(output)
+# print(pl_sentence('this is a test'))

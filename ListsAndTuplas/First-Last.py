f4187da86acbde031cdd1f27f7f15373e0842ce4
garzo94 <alexgarzo25@gmail.com>
2022-09-09 17:07:58 -0600

    Frist and last excersise

---
+++ ListsAndTuplas/First-Last.py
@@ -0,0 +1,18 @@
+# when you retrieve a slice from an object x, you get back a new
+# object of the same type as x. But if you retrieve an individual element from x, you’ll get
+# whatever was stored in x—which might be the same type as x, but you can’t be sure
+
+
+# my solution:
+seq = ('a', 'b', 'c', 'd')
+
+def firstlat(sequence):
+    return sequence[:1] + sequence[-1:]
+
+print(firstlat('abc'))
+
+#book solution
+def firstlast(sequence):
+    return sequence[:1] + sequence[-1:]
+print(firstlast('abcd'))
+
---
+++ Strings/Sorting_string.py
@@ -0,0 +1,12 @@
+# mysoluiton
+def strsort(str):
+    sort_list = sorted(str)
+    return ''.join(sort_list)
+
+print(strsort('bca'))
+
+#book solution
+# def strsort(a_string):
+#     return ''.join(sorted(a_string))
+# print(strsort('cbjeaf'))
+

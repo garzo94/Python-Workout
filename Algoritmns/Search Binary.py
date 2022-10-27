f76c760ec57691862fb395272ea77a193d85a945
garzo94 <alexgarzo25@gmail.com>
2022-10-27 14:39:01 -0600

    oop done

--- Algoritmns/Search Binary.py
+++ Algoritmns/Search Binary.py
@@ -1,22 +1,31 @@
-def binary_search(list, item):
- low = 0
- high = len(list)-1
- counter=0
- while low <= high:
-    mid = (low + high) # this is not the middle is the total of the array, I think is a mistake.
-    guess = list[mid]
-    counter += 1
-    print(counter)
-    if guess == item:
-        return mid
-    if guess > item:
-        high = mid - 1
-    # else:
-    #     low = mid + 1 --> I removed this because never is going to enter here!
- return None
+"""item is our number to guess inside the list,
+returning the index value where the number was found"""
+
+def binary_search(mylist, item):
+    # low index in our list
+    low = 0
+    # high index in our list
+    high = len(mylist) - 1
+
+    while low <= high:
+        mid = (low + high) // 2 # middle index value in the list
+        guess = mylist[mid] # guess number
+
+        # return index if guess number = item
+        if guess == item:
+            return mid
+        # if guess number is higher than item, discards all the subsequent numbers
+        if guess > item:
+            high = mid - 1
+        # otherwise discards all the previous numbers
+        else:
+            low = mid + 1
+    # return None if value does not exist
+    return None
+
 
 my_list = [1, 3, 5, 7, 9]
-print(binary_search(my_list, -1))
+print(binary_search(my_list, 5))
 
 """
 Suppose you have a sorted list of 128 names, and you’re searching
--- OOP/Zoo.py
+++ OOP/Zoo.py
@@ -1,31 +1,59 @@
-class Sheep:
-    number_of_legs = 4
-    species = 'sheep'
-    def __init__(self,color):
+import random
+
+class Animal():
+    def __init__(self, color, number_of_legs):
+        self.species = self.__class__.__name__ # this gives me a string reprentacion of the class
         self.color = color
+        self.number_of_legs = number_of_legs
+
     def __repr__(self):
-        return f'{self.color} {self.species}, {self.number_of_legs} legs'
+        return f'{self.color} {self.species},{self.number_of_legs} legs'
 
-class Wolves(Sheep):
-    species = 'wolve'
+
+class Wolf(Animal):
     def __init__(self, color):
-        super().__init__(color)
+        super().__init__(color, 4)
 
-class Snakes(Sheep):
-    species = 'snake'
-    number_of_legs = 0
+
+class Sheep(Animal):
     def __init__(self, color):
-        super().__init__(color)
+        super().__init__(color, 4)
 
-class Parrots(Sheep):
-    species = 'parrot'
-    number_of_legs = 2
+
+class Snake(Animal):
     def __init__(self, color):
-        super().__init__(color)
+        super().__init__(color, 0)
 
 
+class Parrot(Animal):
+    def __init__(self, color):
+        super().__init__(color, 2)
 
+class Cage:
+    cageID = random.randint(1,100)
+    cage = []
+    def add_animals(self, *args):
+        for arg in args:
+            self.cage.append(arg)
+    def __repr__(self):
+        return f'ID: {self.cageID}\n' + '\n'.join(animal.species for animal in self.cage)
 
+class Zoo:
+    def __init__(self):
+        self.cages = []
+
+    def add_cages(self,*mycages):
+        for cage in mycages:
+            self.cages.append(cage)
+
+    def animals_by_color(self,color):
+        return [ animal for cage in self.cages for animal in cage if animal.color == color]
+
+    def animals_by_legs(self, legs):
+        return [animal for cage in self.cages for animal in cage if animal.number_of_legs == legs]
+
+    def number_of_legs(self):
+        return sum(animal.number_of_legs for cage in self.cages for animal in cage)
 
 
 
